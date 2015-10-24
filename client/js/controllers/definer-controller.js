594e5a676e469a1b389ad9422f51b5bf9592da60
Aryaman Sharda <asharda@ucsd.edu>
2015-10-23 20:51:18 -0700

    Fixing colors and respective formatting.

--- client/js/controllers/definer-controller.js
+++ client/js/controllers/definer-controller.js
@@ -3,16 +3,17 @@ function definerController($scope) {
 
   $scope.defineWords = function () {
 
-    alert("Hello! I am an alert box!!");
+
 
     console.log($scope.definitionWord);
     console.log("hi");
 
       $.ajax({
-        url: "http://api.wordnik.com:80/v4/word.json/dog/definitions?limit=1&includeRelated=true&sourceDictionaries=all&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5",
+        url: "http://api.wordnik.com:80/v4/word.json/" + $scope.definitionWord + "/definitions?limit=1&includeRelated=true&sourceDictionaries=all&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5",
         dataType: 'jsonp',
         success: function(results){
             console.log(results);
+            alert(results[0].text);
         }
       });
    }
