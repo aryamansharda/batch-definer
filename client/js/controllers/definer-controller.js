d499628bee0ce2f27cbdbbac28a869e5f4820a5f
Aryaman Sharda <asharda@ucsd.edu>
2015-10-24 00:05:51 -0700

    Adding style and batch definition functionality. Code cleanup.

--- client/js/controllers/definer-controller.js
+++ client/js/controllers/definer-controller.js
@@ -1,23 +1,20 @@
 function definerController($scope) {
-  $scope.meetupsCount =  10;
 
   $scope.defineWords = function () {
+    var ajaxComplete = 0;
+    var wordList = $scope.definitionWord.split("\n");
+    $scope.definitionWord = "";
 
-
-
-    console.log($scope.definitionWord);
-    console.log("hi");
-
+    for (i = 0; i < wordList.length; i++) {
       $.ajax({
-        url: "http://api.wordnik.com:80/v4/word.json/" + $scope.definitionWord + "/definitions?limit=1&includeRelated=true&sourceDictionaries=all&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5",
+        url: "http://api.wordnik.com:80/v4/word.json/" + wordList[i] + "/definitions?limit=1&includeRelated=true&sourceDictionaries=all&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5",
         dataType: 'jsonp',
-        success: function(results){
-            console.log(results);
-            alert(results[0].text);
+        success: function(results) {
+            ajaxComplete++;
+            $scope.definitionWord += wordList[ajaxComplete - 1] + ": " + results[0].text + "\n";
+            $scope.$apply();
         }
       });
+    }
    }
-
-
-
 }
